8810cfa2b67c48157c8e14b8098ea443ba451f98
Daniel Berger <djberg96@gmail.com>
2015-08-19 16:52:49 -0600

    Set VERSION constant here.

--- lib/sys/uname.rb
+++ lib/sys/uname.rb
@@ -1,3 +1,15 @@
+module Sys
+  class Uname
+    # The version of the sys-uname gem.
+    VERSION = "1.0.1"
+  end
+
+  class Platform
+    # The version of the sys-uname gem.
+    VERSION = Uname::VERSION
+  end
+end
+
 if File::ALT_SEPARATOR
   require_relative 'windows/uname'
 else
