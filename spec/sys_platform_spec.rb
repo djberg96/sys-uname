9871f29d7daedb1fc90d5cf759bd4cd41793a20e
Daniel Berger <djberg96@gmail.com>
2020-01-05 07:56:09 -0500

    Convert the sys-platform tests to rspec.

--- spec/sys_platform_spec.rb
+++ spec/sys_platform_spec.rb
@@ -8,68 +8,68 @@ require 'sys/uname'
 require 'rbconfig'
 
 RSpec.describe Sys::Platform do
-  let(:host_os){ RbConfig::CONFIG['host_os'] }
-  let(:windows){ RbConfig::CONFIG['host_os'] =~ /mingw|mswin|windows/i ? true : false }
+
+  before(:context) do
+    @host_os = RbConfig::CONFIG['host_os']
+    @windows = @host_os =~ /mingw|mswin|windows/i ? true : false
+  end
 
   example "the VERSION constant is set to the expected value" do
     expect(Sys::Platform::VERSION).to eql('1.2.0')
     expect(Sys::Platform::VERSION).to be_frozen
   end
 
-=begin
-  test "the ARCH constant is defined" do
-    assert_kind_of(Symbol, Sys::Platform::ARCH)
+  example "the ARCH constant is defined" do
+    expect(Sys::Platform::ARCH).to be_kind_of(Symbol)
   end
 
-  test "the OS constant is defined" do
-    assert_kind_of(Symbol, Sys::Platform::OS)
+  example "the OS constant is defined" do
+    expect(Sys::Platform::OS).to be_kind_of(Symbol)
   end
 
-  test "the IMPL constant is defined" do
-    assert_kind_of(Symbol, Sys::Platform::IMPL)
+  example "the IMPL constant is defined" do
+    expect(Sys::Platform::IMPL).to be_kind_of(Symbol)
   end
 
-  test "the IMPL returns an expected value" do
-    omit_unless(@@windows)
-    assert_true([:mingw, :mswin].include?(Sys::Platform::IMPL))
+  example "the IMPL returns an expected value", :if => @windows do
+    expect(Sys::Platform::IMPL).to include([:mingw, :mswin])
   end
 
-  test "the mac? method is defined and returns a boolean" do
-    assert_respond_to(Sys::Platform, :mac?)
-    assert_boolean(Sys::Platform.mac?)
+  example "the mac? method is defined and returns a boolean" do
+    expect(Sys::Platform).to respond_to(:mac?)
+    expect(Sys::Platform.mac?).to eql(true).or eql(false)
   end
 
-  test "the windows? method is defined and returns a boolean" do
-    assert_respond_to(Sys::Platform, :windows?)
-    assert_boolean(Sys::Platform.windows?)
+  example "the windows? method is defined and returns a boolean" do
+    expect(Sys::Platform).to respond_to(:windows?)
+    expect(Sys::Platform.windows?).to eql(true).or eql(false)
   end
 
-  test "the windows? method returns the expected value" do
-    assert_equal(Sys::Platform.windows?, @@windows)
+  example "the windows? method returns the expected value" do
+    expect(Sys::Platform.windows?).to eql(@windows)
   end
 
-  test "the unix? method is defined and returns a boolean" do
-    assert_respond_to(Sys::Platform, :unix?)
-    assert_boolean(Sys::Platform.unix?)
+  example "the unix? method is defined and returns a boolean" do
+    expect(Sys::Platform).to respond_to(:unix?)
+    expect(Sys::Platform.unix?).to eql(true).or eql(false)
   end
 
-  test "the unix? method returns the expected value" do
-    assert_equal(Sys::Platform.unix?, !@@windows)
+  example "the unix? method returns the expected value" do
+    expect(Sys::Platform.unix?).not_to eql(@windows)
   end
 
-  test "the solaris? method is defined and returns a boolean" do
-    assert_respond_to(Sys::Platform, :solaris?)
-    assert_boolean(Sys::Platform.solaris?)
+  example "the solaris? method is defined and returns a boolean" do
+    expect(Sys::Platform).to respond_to(:solaris?)
+    expect(Sys::Platform.solaris?).to eql(true).or eql(false)
   end
 
-  test "the linux? method is defined and returns a boolean" do
-    assert_respond_to(Sys::Platform, :linux?)
-    assert_boolean(Sys::Platform.linux?)
+  example "the linux? method is defined and returns a boolean" do
+    expect(Sys::Platform).to respond_to(:linux?)
+    expect(Sys::Platform.linux?).to eql(true).or eql(false)
   end
 
-  test "the bsd? method is defined and returns a boolean" do
-    assert_respond_to(Sys::Platform, :bsd?)
-    assert_boolean(Sys::Platform.bsd?)
+  example "the bsd? method is defined and returns a boolean" do
+    expect(Sys::Platform).to respond_to(:bsd?)
+    expect(Sys::Platform.bsd?).to eql(true).or eql(false)
   end
-=end
 end
