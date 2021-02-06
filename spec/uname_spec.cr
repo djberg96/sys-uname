0404ae52126cb9685d4e15b418e31ffda7ae6778
Daniel Berger <djberg96@gmail.com>
2021-02-06 13:32:22 -0500

    Add more specs.

--- spec/uname_spec.cr
+++ spec/uname_spec.cr
@@ -3,6 +3,30 @@ require "./spec_helper.cr"
 describe System do
   it "correctly reports the operating system name" do
     expected = `uname -s`.chomp
-    System.sysname.should eq expected
+    System.sysname.should eq(expected)
+  end
+
+  it "correctly reports the nodename" do
+    expected = `uname -n`.chomp
+    System.nodename.should eq(expected)
+  end
+
+  it "correctly reports the machine name" do
+    expected = `uname -m`.chomp
+    System.machine.should eq(expected)
+  end
+
+  it "correctly reports the release name" do
+    expected = `uname -r`.chomp
+    System.release.should eq(expected)
+  end
+
+  it "correctly reports the version" do
+    expected = `uname -v`.chomp
+    System.version.should eq(expected)
+  end
+
+  it "returns a struct if the uname method is used" do
+    System.uname.should be_a(System::Uname)
   end
 end
